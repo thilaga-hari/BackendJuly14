e59486f8f18b9dcc624137122095b8ca4aa481dd
thilaga-hari <45123679+thilaga-hari@users.noreply.github.com>
2025-07-25 08:58:37 +0530

    Update Client.java
    ptint 1 - 100 threads in any order

--- src/introToMultiThreading/Client.java
+++ src/introToMultiThreading/Client.java
@@ -7,18 +7,10 @@ public class Client
         System.out.println("Current thread: " + Thread.currentThread().getName());
 
         HelloWorldPrinter helloWorldPrinter = new HelloWorldPrinter();
-//        helloWorldPrinter.print();
-
-        Thread thread = new Thread(helloWorldPrinter);
-        thread.start();
-
-        Thread thread2 = new Thread(helloWorldPrinter);
-        thread2.start();
-
-        Thread thread3= new Thread(helloWorldPrinter);
-        thread3.start();
-
-        Thread thread4 = new Thread(helloWorldPrinter);
-        thread4.start();
+//       helloWorldPrinter.print();
+         for (int i = 1; i <= 100; i++) {
+             Thread thread = new Thread(helloWorldPrinter);
+             thread.start();
+        }
     }
 }
